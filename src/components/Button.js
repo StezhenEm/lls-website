45b39f328f578a982fb259e3fecfd97f6658b215
Stephen Emoghene <stezheh@gmail.com>
2023-08-31 13:30:56 -0400

    get started

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Button.css';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 const STYLES = ['btn--primary', 'btn--outline'];
 
@@ -10,16 +10,19 @@ export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
+    function openURL() {
+        const url = 'https://docs.google.com/forms/d/e/1FAIpQLScM2LTDK4evWr8030ahSTsgJ2gpMHeKMsYvq___Pr49_MJ8lQ/viewform';
+        window.open(url);
+    }
     return (
-        <Link to='/donate' className='btn=mobile'>
+        <div className='btn=mobile'>
             <button
             className ={`btn ${checkButtonStyle} ${checkButtonSize}`}
-            onClick={onClick}
+            onClick={openURL}
             type={type}
             >
                 {children}
             </button>
-        </Link>
+        </div>
     );
 };
