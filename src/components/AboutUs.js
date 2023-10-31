ca1c8269bbf8fe0cf075b98c18c9a02f607b14b7
Stephen Emoghene <stezheh@gmail.com>
2023-10-31 12:55:38 -0400

    logo removed for now

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,32 +3,10 @@ import '../App.css';
 import './AboutUs.css';
 
 function AboutUs() {
-    /*
-    const lukePic = new URL("../image/Luke.jpg", import.meta.url)
-    const nikhilPic = new URL("../image/Nikihal.jpg", import.meta.url)
-    const sohanPic = new URL("../image/Sohan.jpg", import.meta.url)
-    const kentPic = new URL("../image/Kent.jpg", import.meta.url)
-    const drewPic = new URL("../image/Drewski.jpg", import.meta.url)
-    const stephenPic = new URL("../image/Stephen.jpg", import.meta.url)
-    const yeboahPic = new URL("../image/yeboahPic.jpg", import.meta.url)
-    const justinPic = new URL("../image/Jobi.jpg", import.meta.url)
-    
-    const executives = [
-        {image: sohanPic, text: "Sohan, Role: President"},
-        {image: lukePic, text: "Luke Majors Role: Vice President"},
-        {image: nikhilPic, text: "Nikhil Murari, Role: Treasurer"},
-        {image: yeboahPic, text: "Nathan Yeboah, Role: Secretary "},
-        {image: kentPic, text: "Kent Herlihy, Role: Marketing Manager"},
-        {image: stephenPic, text: "Stephen Emoghene, Role: Technology Director"},
-        {image: drewPic, text: "Drew Lancaster, Role: Communications Director"},
-        {image: justinPic, text: "Justin Obimpeh, Role: Research and Eductaion Coordinator"}
-    ];
-    */
-    const logo = new URL("../image/logo.png", import.meta.url)
+
 
     return(
         <div className='about-container' >
-            <img src={logo} alt='Adventure'/> 
             <h1>Our Executive Board</h1>
             <ul>
         <li>Sohan - President</li>
