273e413cc31ecba015edfbeaec1afc11fe79c7ab
Stephen Emoghene <stezheh@gmail.com>
2023-10-31 12:22:05 -0400

    about us spacing

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -28,15 +28,20 @@ function AboutUs() {
     return(
         <div className='about-container' >
             <h1>Our Executive Board</h1>
-            <p>Sohan - President, Luke Majors - Vice President, 
-         Nikhil Murari - Treasurer, Nathan Yeboah - Secretary, Kent Herlihy - Marketing Manager, 
-        Stephen Emoghene - Technology Director, 
-        Drew Lancaster - Communications Director, 
-        Justin Obimpeh - Research and Eductaion Coordinator, 
-        Areen Nagle - Social Media Chair, 
-        Ketan Morrison - Fundraising Chair, 
-        Edgrad Diaz - Co-Advocacy/Policy Chair, 
-        Shaun Price - Co-Advocacy/Policy Chair</p>
+            <ul>
+        <li>Sohan - President</li>
+        <li>Luke Majors - Vice President</li>
+        <li>Nikhil Murari - Treasurer</li>
+        <li>Nathan Yeboah - Secretary</li>
+        <li>Kent Herlihy - Marketing Manager</li>
+        <li>Stephen Emoghene - Technology Director</li>
+        <li>Drew Lancaster - Communications Director</li>
+        <li>Justin Obimpeh - Research and Eductaion Coordinator</li>
+        <li>Areen Nagle - Social Media Chair</li>
+        <li>Ketan Morrison - Fundraising Chair</li>
+        <li>Edgrad Diaz - Co-Advocacy/Policy Chair</li>
+        <li>Shaun Price - Co-Advocacy/Policy Chair</li>
+    </ul>
         </div>
 
     );
