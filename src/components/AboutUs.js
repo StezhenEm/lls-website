556d7e1b4b8dd35cbe55ad7376388be327010132
sohan-uga <ss24012@uga.edu>
2023-12-16 18:23:13 -0500

    About Us and Pillars pages look slightly better

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,28 +2,52 @@ import React from 'react';
 import '../App.css';
 import './AboutUs.css';
 
+import sohanImage from "../image/Sohan.jpg"
+import lukeImage from "../image/Luke.jpg"
+import nikhilImage from "../image/Nikihal.jpg"
+import nathanImage from "../image/yeboahPic.jpg"
+import kentImage from "../image/Kent.jpg"
+import stephenImage from "../image/Stephen.jpg"
+import drewImage from "../image/Drewski.jpg"
+import justinImage from "../image/Jobi.jpg"
+import areenImage from "../image/areen.png"
+import ketanImage from "../image/ketan.png"
+import edgardImage from "../image/edgard.png"
+import shaunImage from "../image/shaun.png"
+
 function AboutUs() {
+    const boardMembers = [
+        {name: 'Sohan', position: 'President', imageUrl:sohanImage},
+        {name: 'Luke', position: 'Vice-President', imageUrl:lukeImage},
+        {name: 'Nikhil', position: 'Treasurer', imageUrl:nikhilImage},
+        {name: 'Nathan', position: 'Secretary', imageUrl:nathanImage},
+        {name: 'Kent', position: 'Events and Outreach Coordinator', imageUrl:kentImage},
+        {name: 'Stephen', position: 'Technology Director', imageUrl:stephenImage},
+        {name: 'Drew', position: 'Communications Director', imageUrl:drewImage},
+        {name: 'Justin', position: 'Research and Education Coordinator', imageUrl:justinImage},
+        {name: 'Areen', position: 'Social Media and Marketing Coordinator', imageUrl:areenImage},
+        {name: 'Ketan', position: 'Fundraising Chair', imageUrl:ketanImage},
+        {name: 'Edgard', position: 'Advocacy and Policy Co-Chair', imageUrl:edgardImage},
+        {name: 'Shaun', position: 'Advocacy and Policy Co-Chair', imageUrl:shaunImage},
+    ]
 
-
-    return(
-        <div className='about-container' >
+    return (
+        <div className='about-container'>
             <h1>Our Executive Board</h1>
-            <ul>
-        <li>Sohan - President</li>
-        <li>Luke Majors - Vice President</li>
-        <li>Nikhil Murari - Treasurer</li>
-        <li>Nathan Yeboah - Secretary</li>
-        <li>Kent Herlihy - Marketing Manager</li>
-        <li>Stephen Emoghene - Technology Director</li>
-        <li>Drew Lancaster - Communications Director</li>
-        <li>Justin Obimpeh - Research and Eductaion Coordinator</li>
-        <li>Areen Nagle - Social Media Chair</li>
-        <li>Ketan Morrison - Fundraising Chair</li>
-        <li>Edgard Diaz - Co-Advocacy/Policy Chair</li>
-        <li>Shaun Price - Co-Advocacy/Policy Chair</li>
-    </ul>
+            <div className="board-grid">
+                {boardMembers.map(member => (
+                    <div className="board-member" key={member.name}>
+                        <div className="board-image-container">
+                            <img src={member.imageUrl} alt={member.name} className="board-image"/>
+                        </div>
+                        <div className="board-info">
+                            <strong>{member.position}</strong>
+                            <p>{member.name}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-
     );
 }
 
--- src/components/Pillars.js
+++ src/components/Pillars.js
@@ -2,12 +2,21 @@ import React from 'react';
 import '../App.css';
 import './Pillars.css';
 
-const pillar = new URL("../image/Pillars.jpg", import.meta.url)
 function Pillars() {
     return(
-        <div className='body-container' >
-            <img src={pillar} alt='pillars'/> 
-            
+        <div className='pillar-container' >
+            <div className="pillar-item">
+                <h2>Awareness</h2>
+                <p>Our club is dedicated to raising awareness about blood cancers such as leukemia and lymphoma. We do this through events, social media sharing, and advocacy for local LLS policies.</p>
+            </div>
+            <div className="pillar-item">
+                <h2>Fundraising</h2>
+                <p>Our club aims to raise funds and attract volunteers for our cause through various school fundraisers such as the Light the Night Event.</p>
+            </div>
+            <div className="pillar-item">
+                <h2>Patient Support</h2>
+                <p>Our club fosters a supportive community for those impacted by blood cancers, promoting belonging and equality. We work with local hospitals for patient care opportunities.</p>
+            </div>
         </div>
     );
 }
